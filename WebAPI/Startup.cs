8b3d2f271596ce2be47388f229bbd33e335db588
dmlkcncat <damla.akcin11@gmail.com>
2023-04-24 14:03:09 +0300

    son

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,15 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
 using Microsoft.AspNetCore.Builder;
 
 namespace WebAPI
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app)
+        public Startup(IConfiguration configuration)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IAboutUsService, AboutUsManager>();
+            services.AddSingleton<IAboutUsDal, AboutUsDal>();
+
+            services.AddSingleton<ICategoryService, CategoryManager>();
+            services.AddSingleton<ICategoryDal, CategoryDal>();
+
+            services.AddSingleton<IContactFormService, ContactFormManager>();
+            services.AddSingleton<IContactFormDal, ContactFormDal>();
+
+            services.AddSingleton<IContactInformationService, ContactInformationManager>();
+            services.AddSingleton<IContactInformationDal, ContactInformationDal>();
+
+            services.AddSingleton<IFeaturesService, FeaturesManager>();
+            services.AddSingleton<IFeaturesDal, FeaturesDal>();
+
+            services.AddSingleton<IImageService, ImageManager>();
+            services.AddSingleton<IImageDal, ImageDal>();
+
+            services.AddSingleton<IProductService, ProductManager>();
+            services.AddSingleton<IProductDal, ProductDal>();
+
+            services.AddSingleton<IRequestFeatureService, RequestFeatureManager>();
+            services.AddSingleton<IFeaturesDal, FeaturesDal>();
+
+            services.AddSingleton<IUserService, UserManager>();
+            services.AddSingleton<IUserDal, UserDal>();
+
+        }
+
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPP v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors(x => x
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin()
+            );
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
